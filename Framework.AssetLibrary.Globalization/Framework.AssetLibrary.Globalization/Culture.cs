e038cbe9cbcb7816533133370d9314284d4aaa82
ram.sarangapany <ram.sarangapany@reedonline.co.uk>
2017-05-23 13:11:31 +0100

    Fixed culture not found exception

--- Framework.AssetLibrary.Globalization/Framework.AssetLibrary.Globalization/Culture.cs
+++ Framework.AssetLibrary.Globalization/Framework.AssetLibrary.Globalization/Culture.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace Framework.AssetLibrary.Globalization
 {
@@ -20,98 +21,111 @@ namespace Framework.AssetLibrary.Globalization
         /// Gets the region information.
         /// </summary>
         /// <value>The region information.</value>
-        internal RegionInfo _RegionInfo => new RegionInfo(CultureInfo.Name);
+        internal RegionInfo RegionInfo => CultureInfo != null ? new RegionInfo(CultureInfo.Name) : null;
 
 
         /// <summary>
         /// Gets the culture information.
         /// </summary>
         /// <value>The culture information.</value>
-        internal CultureInfo CultureInfo => new CultureInfo(Language.LanguageCodeTwoLetter + "-" + Country.CountryCodeTwoLetter);
+        internal CultureInfo CultureInfo
+        {
+            get
+            {
+                try
+                {
+                    return new CultureInfo(Language.LanguageCodeTwoLetter + "-" + Country.CountryCodeTwoLetter);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
 
 
         /// <summary>
         /// Gets or sets the name of the country english.
         /// </summary>
         /// <value>The name of the country english.</value>
-        public string CountryEnglishName => _RegionInfo?.EnglishName;
+        public string CountryEnglishName => RegionInfo?.EnglishName;
 
         /// <summary>
         /// Gets or sets the name of the country native.
         /// </summary>
         /// <value>The name of the country native.</value>
-        public string CountryNativeName => _RegionInfo?.NativeName;
+        public string CountryNativeName => RegionInfo?.NativeName;
 
         /// <summary>
         /// Gets or sets the currency code.
         /// </summary>
         /// <value>The currency code.</value>
-        public string CurrencyCode => _RegionInfo?.ISOCurrencySymbol;
+        public string CurrencyCode => RegionInfo?.ISOCurrencySymbol;
 
         /// <summary>
         /// Gets or sets the name of the currency english.
         /// </summary>
         /// <value>The name of the currency english.</value>
-        public string CurrencyEnglishName => _RegionInfo?.CurrencyEnglishName;
+        public string CurrencyEnglishName => RegionInfo?.CurrencyEnglishName;
 
         /// <summary>
         /// Gets or sets the name of the currency native.
         /// </summary>
         /// <value>The name of the currency native.</value>
-        public string CurrencyNativeName => _RegionInfo?.CurrencyNativeName;
+        public string CurrencyNativeName => RegionInfo?.CurrencyNativeName;
 
         /// <summary>
         /// Gets or sets the currency symbol.
         /// </summary>
         /// <value>The currency symbol.</value>
-        public string CurrencySymbol => _RegionInfo?.CurrencySymbol;
+        public string CurrencySymbol => RegionInfo?.CurrencySymbol;
 
         /// <summary>
         /// Gets or sets the culture code.
         /// </summary>
         /// <value>The culture code.</value>
-        public string CultureCode => CultureInfo.Name;
+        public string CultureCode => CultureInfo?.Name;
 
         /// <summary>
         /// Gets or sets the name of the culture english.
         /// </summary>
         /// <value>The name of the culture english.</value>
-        public string CultureEnglishName => CultureInfo.EnglishName;
+        public string CultureEnglishName => CultureInfo?.EnglishName;
 
         /// <summary>
         /// Gets or sets the name of the culture native.
         /// </summary>
         /// <value>The name of the culture native.</value>
-        public string CultureNativeName => CultureInfo.NativeName;
+        public string CultureNativeName => CultureInfo?.NativeName;
 
         /// <summary>
         /// Gets the number format.
         /// </summary>
         /// <value>The number format.</value>
-        public NumberFormatInfo NumberFormat => CultureInfo.NumberFormat;
+        public NumberFormatInfo NumberFormat => CultureInfo?.NumberFormat;
 
         /// <summary>
         /// Gets the date time format.
         /// </summary>
         /// <value>The date time format.</value>
-        public DateTimeFormatInfo DateTimeFormat => CultureInfo.DateTimeFormat;
+        public DateTimeFormatInfo DateTimeFormat => CultureInfo?.DateTimeFormat;
 
         /// <summary>
         /// Gets the text format.
         /// </summary>
         /// <value>The text format.</value>
-        public TextInfo TextFormat => CultureInfo.TextInfo;
+        public TextInfo TextFormat => CultureInfo?.TextInfo;
 
         /// <summary>
         /// Gets the calendar.
         /// </summary>
         /// <value>The calendar.</value>
-        public Calendar Calendar => CultureInfo.Calendar;
+        public Calendar Calendar => CultureInfo?.Calendar;
 
         /// <summary>
         /// Gets a value indicating whether this instance is un known.
         /// </summary>
         /// <value><c>true</c> if this instance is un known; otherwise, <c>false</c>.</value>
-        public bool IsUnKnown => CultureInfo.Name.Contains("Unknown Locale");
+        public bool IsUnKnown => CultureInfo?.Name.Contains("Unknown Locale") ?? true;
     }
 }
